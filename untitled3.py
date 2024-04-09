179a0272f3baecc7e43bf789ea3c298f7a51986e
EsteUY0925 <166396682+Estepan24@users.noreply.github.com>
2024-04-09 13:38:33 -0400

    Add files via upload

---
+++ untitled3.py
@@ -0,0 +1,17 @@
+#slicing/indexing
+
+my_list1 = ["1", "2", "3", "4", "5"] 
+print(my_list1[0:3])
+
+my_list2 = ["1", "2", "3", "4", "5"] 
+print(my_list2[2:5])
+
+
+my_list3 =  ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+even_numbers = my_list3[1::2]
+print(even_numbers)
+
+my_list4 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+print(my_list4[::-1])
+
+my_list5 = ["1", "2", "3", "4", "5"] 
